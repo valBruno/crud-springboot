2d2f1d572a7a7722acf794c831af71f8a20a051c
brunoval <bhsousa@gmail.com>
2022-09-12 15:09:09 -0300

    home controller

---
+++ src/test/java/com/example/crudspringboot/controller/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.crudspringboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public HomeControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void home() throws Exception {
+
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+}
